b4e8af402af45036c5e003f086fcebc5c744e290
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2022-05-11 23:17:41 -0400

    Rlease [05/11/202]

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 // No Max Range
-const Random = Math.random
+var num1 = Math.floor(Math.random();
+console.log(num1)
 
 // Max Range
-const Random = Math.floor(Math.random)*10)
+var num2 = Math.floor(Math.random() * 10);
+console.log(num2)
